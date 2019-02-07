97cbdfc0869a5feb1f5165c9de2b064dc15c4bc5
Dmin <jmdmin0@gmail.com>
2019-02-08 05:00:30 +0900

    chore: project init

---
+++ projects/ng-lib/src/lib/ng-lib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgLibComponent } from './ng-lib.component';
+
+@NgModule({
+  declarations: [NgLibComponent],
+  imports: [
+  ],
+  exports: [NgLibComponent]
+})
+export class NgLibModule { }
