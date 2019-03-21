59af60f6f7fad0050b817c51e1d02d0fb2329665
Dmin <jmdmin0@gmail.com>
2019-03-22 03:48:47 +0900

    chore: shared module public에 추가

--- projects/ng-lib/src/public_api.ts
+++ projects/ng-lib/src/public_api.ts
@@ -4,9 +4,11 @@
 
 export * from './lib/ng-lib.module';
 export * from './lib/akita';
-export * from './lib/firestore';
-export * from './lib/types';
+export * from './lib/auth';
 export * from './lib/core';
+export * from './lib/firestore';
 export * from './lib/form';
+export * from './lib/shared';
 export * from './lib/table';
+export * from './lib/types';
 
