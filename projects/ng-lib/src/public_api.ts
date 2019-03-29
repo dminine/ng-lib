be26c2ee6c1c23afaacc71c06530c18ed071eef2
Dmin <jmdmin0@gmail.com>
2019-03-29 15:00:40 +0900

    fix: metadata 지워지는 부분 수정

--- projects/ng-lib/src/public_api.ts
+++ projects/ng-lib/src/public_api.ts
@@ -3,12 +3,12 @@
  */
 
 export * from './lib/ng-lib.module';
-export * from './lib/akita';
-export * from './lib/auth';
-export * from './lib/core';
-export * from './lib/firestore';
-export * from './lib/form';
-export * from './lib/shared';
-export * from './lib/table';
-export * from './lib/types';
+export * from './lib/akita/index';
+export * from './lib/auth/index';
+export * from './lib/core/index';
+export * from './lib/firestore/index';
+export * from './lib/form/index';
+export * from './lib/shared/index';
+export * from './lib/table/index';
+export * from './lib/types/index';
 
